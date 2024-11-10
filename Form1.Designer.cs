95c6aaad5a61082d2dcb93c5543db4d1b3d35899
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-11-10 20:20:20 -0300

    full project

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace RandomChoiceSelector {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.option_4 = new System.Windows.Forms.TextBox();
+            this.option_3 = new System.Windows.Forms.TextBox();
+            this.option_2 = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.Label();
+            this.option_1 = new System.Windows.Forms.TextBox();
+            this.finalResult = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(224)))));
+            this.btn_enter.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_enter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(62)))), ((int)(((byte)(135)))));
+            this.btn_enter.Location = new System.Drawing.Point(148, 272);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(246, 43);
+            this.btn_enter.TabIndex = 1;
+            this.btn_enter.Text = "ELEGIR ALEATORIO";
+            this.btn_enter.UseVisualStyleBackColor = false;
+            this.btn_enter.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.option_4);
+            this.panel1.Controls.Add(this.option_3);
+            this.panel1.Controls.Add(this.option_2);
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Controls.Add(this.option_1);
+            this.panel1.Location = new System.Drawing.Point(43, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(447, 245);
+            this.panel1.TabIndex = 2;
+            // 
+            // option_4
+            // 
+            this.option_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(224)))));
+            this.option_4.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option_4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(139)))), ((int)(((byte)(193)))));
+            this.option_4.Location = new System.Drawing.Point(3, 177);
+            this.option_4.Name = "option_4";
+            this.option_4.Size = new System.Drawing.Size(441, 36);
+            this.option_4.TabIndex = 4;
+            this.option_4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // option_3
+            // 
+            this.option_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(224)))));
+            this.option_3.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option_3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(139)))), ((int)(((byte)(193)))));
+            this.option_3.Location = new System.Drawing.Point(3, 135);
+            this.option_3.Name = "option_3";
+            this.option_3.Size = new System.Drawing.Size(441, 36);
+            this.option_3.TabIndex = 3;
+            this.option_3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // option_2
+            // 
+            this.option_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(224)))));
+            this.option_2.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option_2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(139)))), ((int)(((byte)(193)))));
+            this.option_2.Location = new System.Drawing.Point(3, 93);
+            this.option_2.Name = "option_2";
+            this.option_2.Size = new System.Drawing.Size(441, 36);
+            this.option_2.TabIndex = 2;
+            this.option_2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(62)))), ((int)(((byte)(135)))));
+            this.title.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(220)))), ((int)(((byte)(235)))));
+            this.title.Location = new System.Drawing.Point(107, 11);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(265, 37);
+            this.title.TabIndex = 1;
+            this.title.Text = "INGRESÁ TUS OPCIONES";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // option_1
+            // 
+            this.option_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(224)))));
+            this.option_1.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option_1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(139)))), ((int)(((byte)(193)))));
+            this.option_1.Location = new System.Drawing.Point(3, 51);
+            this.option_1.Name = "option_1";
+            this.option_1.Size = new System.Drawing.Size(441, 36);
+            this.option_1.TabIndex = 0;
+            this.option_1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.option_1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // finalResult
+            // 
+            this.finalResult.AutoSize = true;
+            this.finalResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(62)))), ((int)(((byte)(135)))));
+            this.finalResult.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finalResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(224)))));
+            this.finalResult.Location = new System.Drawing.Point(47, 324);
+            this.finalResult.Name = "finalResult";
+            this.finalResult.Size = new System.Drawing.Size(443, 34);
+            this.finalResult.TabIndex = 5;
+            this.finalResult.Text = resources.GetString("finalResult.Text");
+            this.finalResult.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.finalResult.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(62)))), ((int)(((byte)(135)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(542, 380);
+            this.Controls.Add(this.finalResult);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_enter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Random Choice Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.TextBox option_1;
+        private System.Windows.Forms.TextBox option_2;
+        private System.Windows.Forms.TextBox option_4;
+        private System.Windows.Forms.TextBox option_3;
+        private System.Windows.Forms.Label finalResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace RandomChoiceSelector {
+    public partial class Form1 : Form {
+
+        private ArrayList strings;
+        public Form1() {
+            InitializeComponent();
+            strings = new ArrayList();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) {
+
+            if (!string.IsNullOrWhiteSpace(option_1.Text)) {
+                strings.Add(option_1.Text);
+            }
+            if (!string.IsNullOrWhiteSpace(option_2.Text)) {
+                strings.Add(option_2.Text);
+            }
+            if (!string.IsNullOrWhiteSpace(option_3.Text)) {
+                strings.Add(option_3.Text);
+            }
+            if (!string.IsNullOrWhiteSpace(option_4.Text)) {
+                strings.Add(option_4.Text);
+            }
+
+            if (strings.Count == 0) {
+                finalResult.Text = "No ingresaste ningun valor.";
+            }
+            else if (strings.Count == 1) {
+                finalResult.Text = "Ingresaste 1 valor solo.";
+            }
+            else {
+                string result = randomChoice(strings);
+                finalResult.Text = "La opción elegida es: " + result + ".";            
+            } 
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private String randomChoice(ArrayList array) {
+            Random randomSelector = new Random();
+            int result = randomSelector.Next(0, array.Count);
+            return (string)array[result];
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+    }
+}
